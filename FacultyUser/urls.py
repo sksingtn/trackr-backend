eabfb3178c9a7d9dff9c50fa39d8418290b44267
Shiv Kumar Singh <shivaboss5552@gmail.com>
2021-05-07 00:09:34 +0530

    Added some Admin Batch/Student/Faculty Handlingendpoints, Added mechanism for faculty/student onboarding

--- FacultyUser/urls.py
+++ FacultyUser/urls.py
@@ -3,9 +3,7 @@ from . import views as view
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    
+    path('create-account/', view.CreateAccountView.as_view()),
     path('timeline/', view.TimelineView.as_view()),
-
-
 
 ]
--- FacultyUser/views.py
+++ FacultyUser/views.py
@@ -8,22 +8,51 @@ from django.utils import timezone
 
 
 from .serializers import (FacultySlotSerializer,PreviousSlotSerializer,
-                            OngoingSlotSerializer,NextSlotSerializer)
+                            OngoingSlotSerializer,NextSlotSerializer,
+                          InviteTokenVerifySerializer,FacultyPasswordSerializer)
 from .permissions import IsFaculty
 from base.models import Slot
 
+class CreateAccountView(APIView):
+    permission_classes = [AllowAny]
+    
+    def get(self,request):
+        """
+        Verfies token and sends data to be displayed to frontend so that 
+        faculty can be onboarded.
+        """
+        data = InviteTokenVerifySerializer(data=request.query_params)
+        data.is_valid(raise_exception=True)
 
+        target_user = data.save()
+        user_info = {'name':target_user.name,'invited_by':target_user.admin.name,
+                    'email':target_user.user.email}
+        return Response({'status':1,'data':user_info},status=status.HTTP_200_OK)
 
+    def post(self,request):
+        password = FacultyPasswordSerializer(data=request.data)
+        password.is_valid(raise_exception=True)
+        password = password.validated_data['password']
+
+        data = InviteTokenVerifySerializer(data=request.data)
+        data.is_valid(raise_exception=True)
+        new_user =  data.save(password=password)
+        assert new_user.status == new_user.VERIFIED
+        return Response({'status': 1, 'data': 'Faculty Account created successfully, login to continue.'}, status=status.HTTP_201_CREATED)
+    
 class TimelineView(APIView):
 
     permission_classes = [IsAuthenticated, IsFaculty]
     #Make a common class for both faculty and student view.
     def get(self,request):
 
-        if not request.profile.admin.active:
+        if not self.request.profile.is_active():
+            raise ValidationError('Your Account has been deleted by the admin!')
+            
+        admin = request.profile.admin
+        if not admin.active:
             raise ValidationError('Admin has paused the classes for all batches!')
-
-        
+      
         currentDateTime = timezone.localtime()
         all_slots = Slot.objects.filter(faculty=request.profile,batch__active=True).select_related(
             'timing', 'batch')
